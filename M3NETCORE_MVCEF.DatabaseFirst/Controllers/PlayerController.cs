136f7ce5d305ff7145b87aec12202af270d12c4c
Luis Chang <luis.chang@outlook.com>
2021-12-01 18:56:46 -0500

    Finish MVC EF Database First

---
+++ M3NETCORE_MVCEF.DatabaseFirst/Controllers/PlayerController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using M3NETCORE_MVCEF.DatabaseFirst.Models;
+
+namespace M3NETCORE_MVCEF.DatabaseFirst.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly QatarDBContext _context;
+
+        public PlayerController(QatarDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Player
+        public async Task<IActionResult> Index()
+        {
+            var qatarDBContext = _context.Player.Include(p => p.PlayerPosition);
+            return View(await qatarDBContext.ToListAsync());
+        }
+
+        // GET: Player/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Player
+                .Include(p => p.PlayerPosition)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // GET: Player/Create
+        public IActionResult Create()
+        {
+            ViewData["PlayerPositionId"] = new SelectList(_context.PlayerPosition, "Id", "Description");
+            return View();
+        }
+
+        // POST: Player/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FullName,Dorsal,Age,PlayerPositionId")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(player);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlayerPositionId"] = new SelectList(_context.PlayerPosition, "Id", "Description", player.PlayerPositionId);
+            return View(player);
+        }
+
+        // GET: Player/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Player.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            ViewData["PlayerPositionId"] = new SelectList(_context.PlayerPosition, "Id", "Description", player.PlayerPositionId);
+            return View(player);
+        }
+
+        // POST: Player/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,Dorsal,Age,PlayerPositionId")] Player player)
+        {
+            if (id != player.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(player);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlayerExists(player.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlayerPositionId"] = new SelectList(_context.PlayerPosition, "Id", "Description", player.PlayerPositionId);
+            return View(player);
+        }
+
+        // GET: Player/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Player
+                .Include(p => p.PlayerPosition)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // POST: Player/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var player = await _context.Player.FindAsync(id);
+            _context.Player.Remove(player);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlayerExists(int id)
+        {
+            return _context.Player.Any(e => e.Id == id);
+        }
+    }
+}
